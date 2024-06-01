d72b15135372904a10d314ae79f1eeda81477dce
SimranDwivedi <dwivedisimran107@gmail.com>
2024-06-01 21:24:10 +0530

    initial_commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
+
+
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Gallery from '../components/Gallery'
+const Body = () => {
+  return (
+    <>
+    <Gallery/>
+    <div>Body</div>
+    </>
+    
+  )
+}
+
+export default Body
