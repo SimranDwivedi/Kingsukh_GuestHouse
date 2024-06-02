ba57b4909c5e595f49b61aae1b558346e7f4748e
SimranDwivedi <dwivedisimran107@gmail.com>
2024-06-02 20:54:16 +0530

    add_header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>HTML</div>
+    <>
+    <header class="header">
+        <div class="logo">Kingsukh<br/>Guest House</div>
+        <div class="header-menu">
+            <a href="" class="menu-item">Home</a>
+            <a href="" class="menu-item">About</a >
+            <a href="" class="menu-item">Contact</a>
+            <a href="" class="menu-item">Services</a>
+            <a href="" class="menu-item">Account</a>
+        </div>
+        <div class="book-now">
+            <button>Book Now</button>
+        </div>
+    </header>
+
+    <div class="main-banner">
+        <img src="x2.png" alt=""/>
+    </div>
+
+    </>
+    
   )
 }
 
